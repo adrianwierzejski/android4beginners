9d0b872ffd34202d59ad1dbbb7e85a8e6c9716e4
Adrian Wierzejski <adrianw27a@hotmail.com>
2018-11-28 04:29:10 +0100

    Praca domowa do zajęć nr 3

---
+++ A4BHomework3/app/src/main/java/com/daftmobile/a4bhomework3/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.daftmobile.a4bhomework3
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val REQUEST_SELECT_CONTACT = 1
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        EMAIL_RETRIEVER = EmailRetriever.Impl(applicationContext)
+        sendMailButton.setOnClickListener(this::onButtonClicked)
+    }
+
+    private fun onButtonClicked(viev:View){
+
+        val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_CONTACT)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK) {
+            val contactUri: Uri = data?.data!!
+            val email= EMAIL_RETRIEVER.retrieve(contactUri)
+            sendEmail(arrayOf(email),"Wiadomość z pracy domowej")
+        }
+    }
+    fun sendEmail(addresses: Array<String?>,subject :String){
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, addresses)
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+
+        }
+        Log.d("myapp","sfsdfsdfs")
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+    companion object {
+        lateinit var EMAIL_RETRIEVER: EmailRetriever
+    }
+}
